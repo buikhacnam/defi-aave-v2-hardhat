d4b0536379aee9a8b2437202da959ab979b8f94c
Bui Nam <buikhacnam11@gmail.com>
2022-07-25 14:23:48 +0700

    deposit to lending pool

--- scripts/aaveBorrow.ts
+++ scripts/aaveBorrow.ts
@@ -1,33 +1,48 @@
-import { getWeth } from './getWeth'
+import { AMOUNT3, getWeth } from './getWeth'
 import { ethers, getNamedAccounts } from 'hardhat'
+import { BigNumber } from 'ethers'
 
 async function main() {
 	// the protocol treats everything as an ERC20 token
-	await getWeth()
+	const MyAmount = await getWeth()
 	const { deployer } = await getNamedAccounts()
 
 	/* Now depositing into aave
 		We need to interact with aave protocol, so we need address and abi
 
 		Lending Pool Address Provider:
-			Documentation: 
-				https://docs.aave.com/developers/v/2.0/the-core-protocol/addresses-provider
-				https://docs.aave.com/developers/v/2.0/deployed-contracts/deployed-contracts 
-			Contract address: 0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5
-			abi: https://docs.aave.com/developers/v/2.0/the-core-protocol/addresses-provider/ilendingpooladdressesprovider
+			-	Documentation: 
+					https://docs.aave.com/developers/v/2.0/the-core-protocol/addresses-provider
+					https://docs.aave.com/developers/v/2.0/deployed-contracts/deployed-contracts 
+			-	Contract address: 0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5
+			-	abi: https://docs.aave.com/developers/v/2.0/the-core-protocol/addresses-provider/ilendingpooladdressesprovider
 
 		Lending Pool:
-			Documentation:
-				https://docs.aave.com/developers/v/2.0/the-core-protocol/lendingpool
-				https://docs.aave.com/developers/v/2.0/the-core-protocol/lendingpool/ilendingpool
-			Contract address:  function getLendingPool() external view returns (address); FROM ILendingPoolAddressesProvider
-			abi: https://docs.aave.com/developers/v/2.0/the-core-protocol/lendingpool/ilendingpool
+			-	Documentation:
+					https://docs.aave.com/developers/v/2.0/the-core-protocol/lendingpool
+					https://docs.aave.com/developers/v/2.0/the-core-protocol/lendingpool/ilendingpool
+			-	Contract address:  function getLendingPool() external view returns (address); FROM ILendingPoolAddressesProvider
+			-	abi: https://docs.aave.com/developers/v/2.0/the-core-protocol/lendingpool/ilendingpool
+			-	The lending pool contract: https://github.com/aave/protocol-v2/blob/master/contracts/protocol/lendingpool/LendingPool.sol
 	*/
 
+	// 1. Get the lending pool contract address
 	const lendingPool = await getLendingPool(deployer)
 	// console.log("lendingPool in main: ", lendingPool)
-	console.log("lendingPool.address in main: ", lendingPool.address) // https://etherscan.io/address/0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9#code
-}
+	console.log('lendingPool.address in main: ', lendingPool.address) // https://etherscan.io/address/0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9#code
+
+	/* 
+	2. Deposit into the lending pool 
+	*/
+
+	// 2.1 Approve the aave contract to deposit
+	const wethTokenAddress = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2' // WETH token address of mainet
+	await approveErc20(wethTokenAddress, lendingPool.address, MyAmount, deployer)
+
+	console.log('Depositing into lending pool...')
+	await lendingPool.deposit(wethTokenAddress, MyAmount, deployer, 0) //https://github.com/aave/protocol-v2/blob/master/contracts/protocol/lendingpool/LendingPool.sol
+	console.log("Deposited into lending pool")
+}	
 
 async function getLendingPool(deployer: string) {
 	const getLendingPoolAddressesProvider = await ethers.getContractAt(
@@ -35,12 +50,41 @@ async function getLendingPool(deployer: string) {
 		'0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5',
 		deployer
 	)
-	
+
 	const lendingPoolAddress = await getLendingPoolAddressesProvider.getLendingPool()
 
-	const lendingPool = await ethers.getContractAt('ILendingPool', lendingPoolAddress, deployer)
+	const lendingPool = await ethers.getContractAt(
+		'ILendingPool',
+		lendingPoolAddress,
+		deployer
+	)
 
 	return lendingPool
+}
+
+async function approveErc20(
+	erc20Address: string,
+	spenderAddress: string,
+	amountToSpend: BigNumber,
+	account: string
+) {
+	console.log("start approve erc20")
+	console.log({
+		erc20Address,
+		spenderAddress,
+		amountToSpend,
+		account,
+	})
+	console.log("prove erc20 in progress...")
+	const erc20Token = await ethers.getContractAt("IERC20", erc20Address, account)
+
+	const tx = await erc20Token.approve(spenderAddress, amountToSpend)
+	
+	await tx.wait(1)
+
+	console.log('tx.hash: ', tx.hash)
+
+	console.log("Approved ERC20 token")
 }
 
 main()
--- scripts/getWeth.ts
+++ scripts/getWeth.ts
@@ -6,14 +6,19 @@
 
 //https://weth.io/
 
+import { BigNumber } from 'ethers'
 import { getNamedAccounts, ethers } from 'hardhat'
 
-const AMOUNT = ethers.utils.parseEther('0.02')
+export const AMOUNT = ethers.utils.parseEther('0.02')
+export const AMOUNT2 = ethers.utils.parseEther('0.03')
+export const AMOUNT3 = ethers.utils.parseEther('0.1')
+
+
 export async function getWeth() {
 
     const { deployer } = await getNamedAccounts()
     console.log("deployer in getWeth: ", deployer)
-    // How call the deposit function of the Weth contract???
+    // How to call the deposit function of the Weth contract???
     // you need abi and contract address of the Weth contract
 
     // Weth contract mainet: 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2
@@ -39,12 +44,13 @@ export async function getWeth() {
     */
 
     const iWeth = await ethers.getContractAt('IWeth', '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2', deployer)
-    console.log("iWeth in getWeth: ", iWeth)
+    // console.log("iWeth in getWeth: ", iWeth)
 
     const tx = await iWeth.deposit({value: AMOUNT})
     console.log("tx in getWeth: ", tx)
 
     await tx.wait(1)
-    const wethBalance = await iWeth.balanceOf(deployer)
+    const wethBalance:BigNumber = await iWeth.balanceOf(deployer)
     console.log("wethBalance in getWeth: ", wethBalance.toString()) //20000000000000000 -> 0.02 ETH
+    return wethBalance
 }   
